bb4ae6b5ab45596091b470f5c3a0dc30cdda64c1
Corey McBride <corey.mcbride@byu.edu>
2018-10-02 14:47:05 -0600

    Initial Commit

---
+++ mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindowform.h"
+#include "osgWidget.h"
+
+#include <QDockWidget>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow{parent},
+    mMainWindowUI{new Ui::MainWindowForm}
+{
+    mMainWindowUI->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete mMainWindowUI;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
