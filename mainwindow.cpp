95b123239355ea2d4af60f4e9c86f72a28b8def1
Corey McBride <prof.mcbride@byu.edu>
2020-10-26 21:25:49 -0600

    Updated to use new osgQOpenGLWidget.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindowform.h"
-#include "osgWidget.h"
 
-#include <QDockWidget>
+#include <osg/Camera>
+#include <osg/Geode>
+#include <osg/Material>
+#include <osg/Shape>
+#include <osg/ShapeDrawable>
+#include <osg/StateSet>
+#include <osgViewer/Viewer>
+#include <osg/PositionAttitudeTransform>
+#include <osgGA/TrackballManipulator>
+#include <osgQOpenGL/osgQOpenGLWidget>
+#include "SphereUpdateCallback.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow{parent},
     mMainWindowUI{new Ui::MainWindowForm}
 {
     mMainWindowUI->setupUi(this);
+    QObject::connect(mMainWindowUI->osgWidget, &osgQOpenGLWidget::initialized, this, &MainWindow::setupOsgView);
 }
 
 MainWindow::~MainWindow()
@@ -21,3 +31,64 @@ void MainWindow::on_actionExit_triggered()
     QApplication::quit();
 }
 
+void MainWindow::setupOsgView()
+{
+    create_camera();
+    create_manipulator();
+    create_scene();
+    create_timer();
+}
+
+void MainWindow::create_camera()
+{
+    float aspectRatio = static_cast<float>( this->width() ) / static_cast<float>( this->height() );
+    auto pixelRatio   = this->devicePixelRatio();
+
+    mMainWindowUI->osgWidget->getOsgViewer()->getCamera()->setClearColor( osg::Vec4( 0.f, 0.f, .5, 1.f ) );
+    mMainWindowUI->osgWidget->getOsgViewer()->getCamera()->setProjectionMatrixAsPerspective( 45.f, aspectRatio, 1.f, 1000.f );
+    mMainWindowUI->osgWidget->getOsgViewer()->getCamera()->setViewMatrixAsLookAt(osg::Vec3d(0.0,-20.0,3.0),osg::Vec3d(0,0,0),osg::Vec3d(0,0,1));
+    mMainWindowUI->osgWidget->getOsgViewer()->getCamera()->setClearMask(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+    mMainWindowUI->osgWidget->getOsgViewer()->getCamera()->getOrCreateStateSet()->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON);
+}
+
+void MainWindow::create_manipulator()
+{
+    osg::ref_ptr<osgGA::TrackballManipulator> manipulator = new osgGA::TrackballManipulator;
+    manipulator->setHomePosition(osg::Vec3d(0.0,-20.0,3.0),osg::Vec3d(0,0,0),osg::Vec3d(0,0,1));
+    mMainWindowUI->osgWidget->getOsgViewer()->setCameraManipulator( manipulator );
+}
+
+void MainWindow::create_scene()
+{
+    osg::Sphere* sphere    = new osg::Sphere( osg::Vec3( 0.f, 0.f, 0.f ), 2.0f );
+    osg::ShapeDrawable* sd = new osg::ShapeDrawable( sphere );
+    sd->setColor( osg::Vec4( 1.f, 0.f, 0.f, 1.f ) );
+    sd->setName( "Sphere" );
+
+    osg::Geode* geode = new osg::Geode;
+    geode->addDrawable( sd );
+    osg::StateSet* stateSet = geode->getOrCreateStateSet();
+    osg::Material* material = new osg::Material;
+    material->setColorMode( osg::Material::AMBIENT_AND_DIFFUSE );
+    stateSet->setAttributeAndModes( material, osg::StateAttribute::ON );
+    stateSet->setMode( GL_DEPTH_TEST, osg::StateAttribute::ON );
+
+    osg::PositionAttitudeTransform *transform = new osg::PositionAttitudeTransform;
+    transform->setPosition(osg::Vec3( 0.f, 0.f, 0.f ));
+    transform->setUpdateCallback(new SphereUpdateCallback());
+    transform->addChild(geode);
+
+    mMainWindowUI->osgWidget->getOsgViewer()->setSceneData(transform);
+}
+
+void MainWindow::create_timer()
+{
+    double framesPerSecond{30};
+    double timeStep{1.0/framesPerSecond};
+    double timerDurationInMilliSeconds{timeStep * 1000};
+    mTimerId=startTimer(timerDurationInMilliSeconds);
+}
+void MainWindow::timerEvent(QTimerEvent *)
+{
+    mMainWindowUI->osgWidget->getOsgViewer()->frame();
+}
